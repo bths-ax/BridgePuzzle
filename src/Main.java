64353ba34edc54ae9543829971ea1682b8a7388a
ax <114583570+bths-ax@users.noreply.github.com>
2023-04-27 08:35:58 -0400

    woohooooooooo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<Integer> crossingTimes = new ArrayList<Integer>();
+
+		while (true) {
+			System.out.print(String.format("Crossing time #%d (-1 to end): ", crossingTimes.size() + 1));
+			int crossingTime = scanner.nextInt();
+			if (crossingTime == -1) break;
+			crossingTimes.add(crossingTime);
+		}
+
+		System.out.print("Maximum time allowed: ");
+		int maximumTime = scanner.nextInt();
+
+		int[] crossingTimesArr = new int[crossingTimes.size()];
+		for (int i = 0; i < crossingTimes.size(); i++)
+			crossingTimesArr[i] = crossingTimes.get(i);
+
+		Puzzle puzzle = new Puzzle(crossingTimesArr, maximumTime);
+		Crossing node = puzzle.solve();
+		if (node == null) {
+			System.out.println("No solution found");
+			return;
+		} else {
+			String direction = "=>";
+			while (node.next != null) {
+				node = node.next;
+				if (node.person2 != -1) {
+					System.out.println(direction + ": Person " + (node.person1 + 1) + " and " + (node.person2 + 1));
+				} else {
+					System.out.println(direction + ": Person " + (node.person1 + 1));
+				}
+				if (direction.equals("=>")) direction = "<=";
+				else direction = "=>";
+			}
+		}
 	}
 }
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 class Crossing {
 	public int person1;
 	public int person2;
@@ -15,24 +13,85 @@ public class Puzzle {
 		this.maximumTime = maximumTime;
 	}
 
-	public boolean solve() {
+	public Crossing solve() {
 		boolean[] left = new boolean[crossingTimes.length];
 		boolean[] right = new boolean[crossingTimes.length];
 		for (int i = 0; i < crossingTimes.length; i++)
 			left[i] = true;
 
-		return recurse(left, right, true);
+		Crossing root = new Crossing();
+		root.person1 = -1;
+		root.person2 = -1;
+
+		if (recurse(root, left, right, true) <= maximumTime) {
+			return root;
+		} else {
+			return null;
+		}
 	}
 
-	public int recurse(boolean[] left, boolean[] right, boolean crossLeft) {
-		int minTime = Integer.MAX_INT;
-
+	public int recurse(Crossing node, boolean[] left, boolean[] right, boolean crossLeft) {
 		if (crossLeft) {
-			// cross 2 people
-		} else {
-			// only cross 1 person with the min time
-		}
+			int minTime = Integer.MAX_VALUE;
+			int minIndex1 = -1;
+			int minIndex2 = -1;
 
-		return minTime;
+			for (int i = 0; i < crossingTimes.length; i++) if (left[i]) {
+				for (int j = i + 1; j < crossingTimes.length; j++) if (left[j]) {
+					left[i] = left[j] = false;
+					right[i] = right[j] = true;
+
+					Crossing next = new Crossing();
+					next.person1 = i;
+					next.person2 = j;
+
+					int time = Math.max(crossingTimes[i], crossingTimes[j]);
+					int nextTime = recurse(next, left, right, false);
+					if (time + nextTime < minTime) {
+						minTime = time + nextTime;
+						minIndex1 = i;
+						minIndex2 = j;
+						node.next = next;
+					}
+
+					left[i] = left[j] = true;
+					right[i] = right[j] = false;
+				}
+			}
+
+			return minTime;
+		} else {
+			boolean allCrossed = true;
+			for (int i = 0; i < crossingTimes.length; i++)
+				if (left[i]) allCrossed = false;
+			if (allCrossed) {
+				return 0;
+			}
+
+			int minTime = Integer.MAX_VALUE;
+			int minIndex = -1;
+
+			for (int i = 0; i < crossingTimes.length; i++) if (right[i]) {
+				right[i] = false;
+				left[i] = true;
+				
+				Crossing next = new Crossing();
+				next.person1 = i;
+				next.person2 = -1;
+
+				int time = crossingTimes[i];
+				int nextTime = recurse(next, left, right, true);
+				if (time + nextTime < minTime) {
+					minTime = time + nextTime;
+					minIndex = i;
+					node.next = next;
+				}
+
+				right[i] = true;
+				left[i] = false;
+			}
+
+			return minTime;
+		}
 	}
 }
